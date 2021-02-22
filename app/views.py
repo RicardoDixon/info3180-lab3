28eacdda1464e6dfb24a056d481fbd18ca00ef90
Ricardo Dixon <rickydixxx@gmail.com>
2021-02-21 20:41:19 -0500

    completed info3180-lab3

--- app/views.py
+++ app/views.py
@@ -7,8 +7,9 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
-
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 ###
 # Routing for your application.
 ###
@@ -22,22 +23,29 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Ricardo Dixon")
 
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
 
+@app.route('/contact', methods=["GET","POST"])
+def contact():
+    contactForm = ContactForm()
 
-# Flash errors from the form if validation fails
-def flash_errors(form):
-    for field, errors in form.errors.items():
-        for error in errors:
-            flash(u"Error in the %s field - %s" % (
-                getattr(form, field).label.text,
-                error
-            ), 'danger')
+    if request.method =="POST":
+
+        if contactForm.validate_on_submit():
+            msg = Message(contactForm.subject.data, sender=(contactForm.name.data,contactForm.email.data), recipients=["7f5f657dcf-dcfdb8@inbox.mailtrap.io"])
+            msg.body = contactForm.message.data
+            mail.send(msg)
+
+            flash('Message sent', 'success')
+            return redirect(url_for('home'))
+
+    return render_template("contact.html", contactForm = contactForm)
+
 
 
 @app.route('/<file_name>.txt')
